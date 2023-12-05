2c36244649c1c76c357e501fbb6d62a123e3f7f5
Lachlan Turner <lachlan.turner@aligent.com.au>
2023-12-05 15:07:50 +1030

    Day 3 Part 2 complete

--- src/Day3.java
+++ src/Day3.java
@@ -12,26 +12,31 @@ public class Day3 {
     public static final Set<Character> NON_SYMBOLS = Set.of('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.');
     public static final Set<Character> NUMBERS = Set.of('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
 
+    public static final char GEAR_SYMBOL = '*';
+
     public static void main(String[] args) {
         ArrayList<String> lines = ReadFileAsArray.execute("./input/day-3.txt");
         ArrayList<Coordinates> symbolCoordinates = new ArrayList<>();
         ArrayList<CoordinatesRange> numberCoordinates = new ArrayList<>();
+        ArrayList<Coordinates> gearCoordinates = new ArrayList<>();
         for (int i = 1; i <= lines.size(); i++) {
             String line = lines.get(i-1);
-            symbolCoordinates.addAll(getSymbolCoordinates(i, line));
+            symbolCoordinates.addAll(getSymbolCoordinates(i, line, false));
+            gearCoordinates.addAll(getSymbolCoordinates(i, line, true));
         }
         for (int i = 1; i <= lines.size(); i++) {
             String line = lines.get(i-1);
             numberCoordinates.addAll(getNumberCoordinates(i, line));
         }
         System.out.println(getAdjacentNumbersSum(symbolCoordinates, numberCoordinates));
+        System.out.println(getGearRatioSums(gearCoordinates, numberCoordinates));
     }
 
-    private static ArrayList<Coordinates> getSymbolCoordinates(int y, String line) {
+    private static ArrayList<Coordinates> getSymbolCoordinates(int y, String line, boolean onlyGears) {
         ArrayList<Coordinates> symbolCoordinates = new ArrayList<>();
         char[] charArray = line.toCharArray();
         for (int i = 1; i <= charArray.length; i++) {
-            if (!NON_SYMBOLS.contains(charArray[i-1])) {
+            if ((onlyGears && GEAR_SYMBOL == charArray[i-1]) || (!onlyGears && !NON_SYMBOLS.contains(charArray[i-1]))) {
                 Coordinates coords = new Coordinates();
                 coords.setX(i);
                 coords.setY(y);
@@ -51,6 +56,27 @@ public class Day3 {
                 sum.addAndGet(n.getNumber());
             }
         }));
+        return sum.get();
+    }
+
+    private static int getGearRatioSums(
+            ArrayList<Coordinates> gearCoordinates,
+            ArrayList<CoordinatesRange> numberCoordinates
+    ) {
+        AtomicInteger sum = new AtomicInteger();
+        gearCoordinates.forEach((g) -> {
+            ArrayList<CoordinatesRange> adjacentNumbers = new ArrayList<>();
+            numberCoordinates.forEach((n) -> {
+                if (n.isAdjacent(g)) {
+                    adjacentNumbers.add(n);
+                }
+            });
+            AtomicInteger ratio = new AtomicInteger(1);
+            if (adjacentNumbers.size() == 2) {
+                adjacentNumbers.forEach((n) -> ratio.set(ratio.get() * n.getNumber()));
+                sum.addAndGet(ratio.get());
+            }
+        });
         return sum.get();
     }
 
