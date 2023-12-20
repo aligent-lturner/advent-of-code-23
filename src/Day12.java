796124bd965b753c688ea931b523cc9f978698ee
Lachlan Turner <lachlan.turner@aligent.com.au>
2023-12-20 12:31:24 +1030

    Day 12 Part 2 complete

--- src/Day12.java
+++ src/Day12.java
@@ -1,40 +1,89 @@
-import Util.GetPossibleSpringArrangements;
 import Util.ReadFileAsArray;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class Day12 {
 
+    private static final char GOOD = '.';
+    private static final char DAMAGED = '#';
     private static final char UNKNOWN = '?';
 
     public static void main(String[] args) {
         List<String> lines = ReadFileAsArray.execute("./input/day-12.txt");
+        long sum = getSumOfArrangements(lines);
+        System.out.println("Part 1: Sum = " + sum);
+        lines = lines.stream().map(Day12::unfoldLine).toList();
+        sum = getSumOfArrangements(lines);
+        System.out.println("Part 2: Sum = " + sum);
+    }
+
+    private static String unfoldLine(String line) {
+        String[] values = line.split("\\s+");
+        StringBuilder springs = new StringBuilder(values[0]);
+        StringBuilder springGroups = new StringBuilder(values[1]);
+        for (int i = 0; i < 4; i++) {
+            springs.append('?').append(values[0]);
+            springGroups.append(",").append(values[1]);
+        }
+
+        return springs + " " + springGroups;
+    }
+
+    private static long getSumOfArrangements(List<String> lines) {
         long sum = 0;
         for (String line : lines) {
             String[] values = line.split("\\s+");
             String springs = values[0];
             int[] springGroups = Stream.of(values[1].split(",")).mapToInt(Integer::parseInt).toArray();
-            List<String> allArrangements = GetPossibleSpringArrangements.execute("", springs.length(), springGroups);
-            long possibleArrangements = allArrangements.stream().filter(arrangement -> matchesPattern(springs, arrangement)).count();
+
+            long possibleArrangements = getNumberOfMatches(springs, springGroups);
+
             sum += possibleArrangements;
         }
-        System.out.println("Part 1: Sum = " + sum);
+        return sum;
     }
 
-    private static boolean matchesPattern(String value, String requiredPattern) {
-        if (value.length() != requiredPattern.length()) {
-            return false;
+    private static long getNumberOfMatches(String springs, int[] springGroups) {
+        StringBuilder statesBuilder = new StringBuilder(".");
+        for (int group : springGroups) {
+            statesBuilder.append("#".repeat(Math.max(0, group)));
+            statesBuilder.append(".");
         }
-        for (int i = 0; i < value.length(); i++) {
-            char nextChar = value.charAt(i);
-            char requiredChar = requiredPattern.charAt(i);
-            if (nextChar != requiredChar) {
-                if (nextChar != UNKNOWN) {
-                    return false;
+        String states = statesBuilder.toString();
+
+        Map<Integer, Long> statesMap = new HashMap<>();
+        statesMap.put(0, 1L);
+        Map<Integer, Long> newMap = new HashMap<>();
+
+        for (char c : springs.toCharArray()) {
+            for (Integer state: statesMap.keySet()) {
+                if (c == UNKNOWN) {
+                    if (state + 1 < states.length()) {
+                        newMap.put(state + 1, newMap.getOrDefault(state + 1, 0L) + statesMap.get(state));
+                    }
+                    if (states.charAt(state) == GOOD) {
+                        newMap.put(state, newMap.getOrDefault(state, 0L) + statesMap.get(state));
+                    }
+                } else if (c == GOOD) {
+                    if (state + 1 < states.length() && states.charAt(state + 1) == GOOD) {
+                        newMap.put(state + 1, newMap.getOrDefault(state + 1, 0L) + statesMap.get(state));
+                    }
+                    if (states.charAt(state) == '.') {
+                        newMap.put(state, newMap.getOrDefault(state, 0L) + statesMap.get(state));
+                    }
+                } else if (c == DAMAGED) {
+                    if (state + 1 < states.length() && states.charAt(state + 1) == DAMAGED) {
+                        newMap.put(state + 1, newMap.getOrDefault(state + 1, 0L) + statesMap.get(state));
+                    }
                 }
             }
+            statesMap = newMap;
+            newMap = new HashMap<>();
         }
-        return true;
+        return statesMap.getOrDefault(states.length() - 1, 0L) +
+                statesMap.getOrDefault(states.length() - 2, 0L);
     }
 }
