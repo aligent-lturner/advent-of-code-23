96cd9525328ddb50229ffe7f4e6957a55fb4dc21
Lachlan Turner <lachlan.turner@aligent.com.au>
2023-12-20 17:17:38 +1030

    Day 13 Part 2 complete

--- src/Day13.java
+++ src/Day13.java
@@ -1,3 +1,4 @@
+import Util.LevenshteinDistance;
 import Util.ReadFileAsArray;
 import Util.TransposeLines;
 
@@ -11,15 +12,24 @@ public class Day13 {
         List<List<String>> patterns = getPatterns(lines);
         long sum = 0;
         for (List<String> pattern : patterns) {
-            long horizontalSymmetryLines = countLinesAboveSymmetryLine(pattern);
+            long horizontalSymmetryLines = countLinesAboveSymmetryLine(pattern, false);
             if (horizontalSymmetryLines > 0) {
                 sum += (horizontalSymmetryLines * 100);
             } else {
-                sum += countLinesAboveSymmetryLine(TransposeLines.execute(pattern));
+                sum += countLinesAboveSymmetryLine(TransposeLines.execute(pattern), false);
             }
         }
         System.out.println("Part 1: sum = " + sum);
-
+        sum = 0;
+        for (List<String> pattern : patterns) {
+            long horizontalSymmetryLines = countLinesAboveSymmetryLine(pattern, true);
+            if (horizontalSymmetryLines > 0) {
+                sum += (horizontalSymmetryLines * 100);
+            } else {
+                sum += countLinesAboveSymmetryLine(TransposeLines.execute(pattern), true);
+            }
+        }
+        System.out.println("Part 1: sum = " + sum);
     }
 
     private static List<List<String>> getPatterns(List<String> lines) {
@@ -41,13 +51,13 @@ public class Day13 {
         return patterns;
     }
 
-    private static long countLinesAboveSymmetryLine(List<String> pattern) {
+    private static long countLinesAboveSymmetryLine(List<String> pattern, boolean offByOne) {
         long linesSum = 0;
         // assume there has to be at least 1 row on each side
         for (int i = 1; i < pattern.size(); i++) {
             List<String> linesAbove = pattern.subList(0, i).reversed();
             List<String> linesBelow = pattern.subList(i, pattern.size());
-            if (presentLinesAreEqual(linesAbove, linesBelow)) {
+            if (presentLinesAreEqual(linesAbove, linesBelow, offByOne)) {
                 linesSum += linesAbove.size();
             }
         }
@@ -55,11 +65,19 @@ public class Day13 {
         return linesSum;
     }
 
-    private static boolean presentLinesAreEqual(List<String> linesAbove, List<String> linesBelow) {
+    private static boolean presentLinesAreEqual(List<String> linesAbove, List<String> linesBelow, boolean offByOne) {
+        int totalDistance = 0;
         for (int i = 0; i < Math.min(linesAbove.size(), linesBelow.size()); i++) {
-            if (!linesAbove.get(i).equals(linesBelow.get(i))) {
-                return false;
+            if (offByOne) {
+                totalDistance += LevenshteinDistance.computeLevenshteinDistance(linesAbove.get(i), linesBelow.get(i));
+            } else {
+                if (!linesAbove.get(i).equals(linesBelow.get(i))) {
+                    return false;
+                }
             }
+        }
+        if (offByOne) {
+            return totalDistance == 1;
         }
         return true;
     }
